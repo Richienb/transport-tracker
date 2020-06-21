571bc592a68ee059ac1978f9748829c0e76bfb05
Richie Bendall <richiebendall@gmail.com>
2020-06-22 00:25:06 +1200

    Fix file resolution
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const ejs = require("ejs")
+const path = require("path")
 
 module.exports = async (_, response) => {
-	response.send(await ejs.renderFile("views/index.ejs"))
+	response.send(await ejs.renderFile(path.resolve(__dirname, "../views/index.ejs")))
 }
