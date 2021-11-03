1f7c602fa312f2c076a4d47d226aadcd08fac8d6
Richie Bendall <richiebendall@gmail.com>
2021-11-03 16:35:53 +1300

    Remove unused font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
 		<Html>
 			<Head>
 				<link rel='icon' href='/favicon.ico' />
-				<link href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Roboto+Condensed:wght@700&display=swap' rel='stylesheet' />
+				<link href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap' rel='stylesheet' />
 			</Head>
 
 			<body>
